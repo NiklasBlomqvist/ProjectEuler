e3fc61333b7060fcf062358eb746709e18d10f4b
Niklas Blomqvist <ntjblomqvist@gmail.com>
2024-09-08 18:44:09 +0200

    Problem 11 WIP

--- Project Euler/Program.cs
+++ Project Euler/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Start of Program.");
 
-            Problem_10.Solve();
+            Problem_11.Solve();
 
             Console.WriteLine("End of Program.");
         }
