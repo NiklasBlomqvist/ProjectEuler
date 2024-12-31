57d868927d83727c71ebd5970bda914cb7d02700
Niklas Blomqvist <ntjblomqvist@gmail.com>
2024-12-31 15:54:43 +0100

    Solution part 21

---
+++ Project Euler/Problem_21.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+
+namespace ProjectEuler;
+
+public static class Problem_21
+{
+    public static void Solve()
+    {
+        var listOfAmicableNumbers = new List<int>();
+        var upperLimit = 10000;
+        for (var i = 1; i < upperLimit; i++)
+        {
+            if(IsAmicableNumber(i))
+                listOfAmicableNumbers.Add(i);
+        }
+
+        Console.WriteLine($"All amicable numbers:");
+        foreach (var n in listOfAmicableNumbers)
+        {
+            Console.WriteLine(n);
+        }
+        
+        var sumOfAllAmicableNumbers = listOfAmicableNumbers.Sum();
+        Console.WriteLine($"Sum of all amicable numbers under {upperLimit} = {sumOfAllAmicableNumbers}");
+    }
+
+    private static bool IsAmicableNumber(int a)
+    {
+        var b = SumOfProperDivisors(a);
+
+        if (a == b)
+            return false;
+        
+        return SumOfProperDivisors(b) == a;
+    }
+    
+    private static int SumOfProperDivisors(int n)
+    {
+        var sum = 0;
+        // Get all divisors of n
+        for (var i = 1; i <= n / 2; i++)
+        {
+            if (n % i == 0)
+            {
+                sum += i;
+            }
+        }
+        
+        // Sum them together and return
+        return sum;
+    }
+}
--- Project Euler/Program.cs
+++ Project Euler/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Start of Program.");
 
-            Problem_20.Solve();
+            Problem_21.Solve();
 
             Console.WriteLine("End of Program.");
         }
